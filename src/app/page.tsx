29af131558113e0ca4574088e6288b92161f63dd
rizkydrma <rizky.darmarazak@gmail.com>
2023-07-25 18:22:01 +0700

    feat: add meta tag and open graph

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+import Seo from '@/components/Seo';
 import Landing from '@/components/landing';
 import Preloader from '@/components/preloader';
 import { AnimatePresence } from 'framer-motion';
@@ -8,15 +9,21 @@ export default function Home() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-      document.body.style.cursor = 'default';
-      window.scrollTo(0, 0);
-    }, 2000);
+    (async () => {
+      const LocomotiveScroll = (await import('locomotive-scroll')).default;
+      const locomotiveScroll = new LocomotiveScroll();
+
+      setTimeout(() => {
+        setLoading(false);
+        document.body.style.cursor = 'default';
+        window.scrollTo(0, 0);
+      }, 2000);
+    })();
   }, []);
 
   return (
     <main>
+      <Seo />
       <AnimatePresence mode="wait">{loading && <Preloader />}</AnimatePresence>
       <Landing />
     </main>
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,35 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+type OpenGraphType = {
+  siteName: string;
+  description: string;
+  templateTitle?: string;
+  logo?: string;
+  banner?: string;
+  isBlog?: boolean;
+};
+export function openGraph({
+  siteName,
+  templateTitle,
+  description,
+  banner,
+  logo = 'https://res.cloudinary.com/de3n7a1r0/image/upload/v1690282671/logo_yf4tu4.png',
+  isBlog = false,
+}: OpenGraphType): string {
+  const ogLogo = encodeURIComponent(logo);
+  const ogSiteName = encodeURIComponent(siteName.trim());
+  const ogTemplateTitle = templateTitle ? encodeURIComponent(templateTitle.trim()) : undefined;
+  const ogDesc = encodeURIComponent(description.trim());
+
+  if (isBlog) {
+    const ogBanner = banner ? encodeURIComponent(banner.trim()) : undefined;
+
+    return `https://og-rizkydrma.vercel.app/api/blog?templateTitle=${ogTemplateTitle}&banner=${ogBanner}`;
+  }
+
+  return `https://og-rizkydrma.vercel.app/api/gradient?siteName=${ogSiteName}&description=${ogDesc}&logo=${ogLogo}${
+    ogTemplateTitle ? `&templateTitle=${ogTemplateTitle}` : ''
+  }`;
+}
