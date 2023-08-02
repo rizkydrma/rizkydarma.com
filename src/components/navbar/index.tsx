fea5be7b935e20c52dd0e9011a7119201a460a58
rizkydrma <rizky.darmarazak@gmail.com>
2023-08-02 08:33:29 +0700

    feat: linking navbar

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,26 +1,35 @@
 'use client';
 import { cn } from '@/lib/utils';
-import { FC, useState } from 'react';
 import { motion } from 'framer-motion';
+import { FC, useState } from 'react';
 import ThemeToggle from '../themetoggle';
-import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 interface NavbarProps {}
 
 let tabs = [
-  { id: 'home', label: 'Home' },
-  { id: 'projects', label: 'Projects' },
-  { id: 'about', label: 'About' },
-  { id: 'blog', label: '✨ Blog' },
+  { id: 'home', label: 'Home', href: '/' },
+  { id: 'projects', label: 'Projects', href: '/projects' },
+  { id: 'about', label: 'About', href: '/about' },
+  { id: 'blog', label: '✨ Blog', href: '/blog' },
 ];
 
 const Navbar: FC<NavbarProps> = ({}) => {
-  const [activeTab, setActiveTab] = useState(tabs?.[0]?.id);
+  // const [activeTab, setActiveTab] = useState(tabs?.[0]?.id);
   return (
     <div className="fixed backdrop-blur-sm z-[9999] top-0 left-0 right-0 h-20 border-b border-stone-200 dark:border-stone-700 shadow-sm flex items-center">
       <div className="z-[9999] container max-w-7xl mx-auto w-full items-center justify-between flex">
         <div className="flex space-x-1">
-          {tabs.map((tab) => (
+          {tabs?.map((tab) => (
+            <Link
+              href={tab?.href}
+              key={tab?.id}
+              className="relative rounded-full px-3 py-1.5 text-sm font-medium text-stone-800  dark:text-white outline-yellow-400 transition focus-visible:outline-2"
+            >
+              {tab?.label}
+            </Link>
+          ))}
+          {/* {tabs.map((tab) => (
             <button
               key={tab.id}
               onClick={() => setActiveTab(tab.id)}
@@ -41,7 +50,7 @@ const Navbar: FC<NavbarProps> = ({}) => {
               )}
               {tab.label}
             </button>
-          ))}
+          ))} */}
         </div>
         <ThemeToggle />
       </div>
