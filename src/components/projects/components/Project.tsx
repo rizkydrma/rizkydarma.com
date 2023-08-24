d770c235f297eca36a399004bfb454b0e21bbc3a
rizkydrma <rizky.darmarazak@gmail.com>
2023-08-24 19:20:38 +0700

    fix: featured project mobile

--- src/components/projects/components/Project.tsx
+++ src/components/projects/components/Project.tsx
@@ -33,7 +33,7 @@ const Project: FC<ProjectProps> = ({ index, title, subtitle, manageModal, tech, 
             }
           : undefined
       }
-      className="flex w-full justify-between items-center py-4 border-t-2 border-t-gray-300 cursor-pointer transition-all text-stone-950 dark:text-stone-50"
+      className="flex w-full flex-col gap-3 lg:flex-row lg:justify-between justify-start lg:items-center items-start py-4 border-t-2 border-t-gray-300 cursor-pointer transition-all text-stone-950 dark:text-stone-50"
     >
       <div>
         <h2 className="text-base lg:text-2xl font-bold">{title}</h2>
@@ -41,8 +41,8 @@ const Project: FC<ProjectProps> = ({ index, title, subtitle, manageModal, tech, 
       </div>
 
       <div>
-        <p className="text-xs tracking-wide text-right pb-2 font-medium">Tech Stack</p>
-        <div className="flex items-center gap-4">
+        <p className="text-xs tracking-wide lg:text-right pb-2 font-medium">Tech Stack</p>
+        <div className="flex items-start lg:items-center gap-4">
           {arrTech?.map((item) => (
             <Image
               key={item}
